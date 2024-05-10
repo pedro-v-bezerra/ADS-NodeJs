a843ba203a6a8ba08b92e068f6dc6ce94ec794d5
Pedro Lima <pedrolima38@hotmail.com>
2024-05-10 19:29:18 -0300

    Aula 10

--- aulas/aula10/tests/router_produtos.test.js
+++ aulas/aula10/tests/router_produtos.test.js
@@ -7,10 +7,10 @@ const request = supertest(app);
 let id = null;
 
 describe('API Loja Virtual', () => {
-    test("Deve Retornar 201  e JSON no POST /produto", async () => {
-        const response = await request.post('/produtos').send({nome: "banana", preco: 12.0})
-        expect(response.status).toBe(201)
-        expect(response.type).toBe('application/json')
-        id = response.body._id
-    })
+    // test("Deve Retornar 201  e JSON no POST /produto", async () => {
+    //     const response = await request.post('/produtos').send({nome: "banana", preco: 12.0})
+    //     expect(response.status).toBe(201)
+    //     expect(response.type).toBe('application/json')
+    //     id = response.body._id
+    // })
 })
